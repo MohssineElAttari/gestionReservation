aba0e6b316b894cd7fa114ecbc90951efeffb1e7
MohssineElAttari <14mohssine@gmail.com>
2021-05-03 11:23:20 +0000

    updete

--- Classes/Reservation.php
+++ Classes/Reservation.php
@@ -1,5 +1,25 @@
 <?php
-
-class Reservation {
-    private id_utilisateur;
+class Reservation
+{
+    private $idUtilisateur;
+    private $idBien;
+    private $idPension;
+    private $dateEntrer;
+    private $dateSortie;
 }
+//constructeur initialisation
+function __construct($idU, $idB, $idP, $dateE, $dateS)
+{
+    $this->idUtilisateur = $idU;
+    $this->idBien = $idB;
+    $this->idPension = $idP;
+    $this->dateEntrer = $dateE;
+    $this->dateSortie = $dateS;
+}
+
+//getter and setter
+public function getIdUtilisateur()
+{
+    return $this->$this->idUtilisateur;
+}
+?>
