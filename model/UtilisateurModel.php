a350fa750a3e0a546b98a6350d1e6e1d25b21a5f
MohssineElAttari <14mohssine@gmail.com>
2021-05-05 11:29:21 +0000

    add utilisateur model

---
+++ model/UtilisateurModel.php
@@ -0,0 +1,52 @@
+<?php
+include_once '../classes/DataBase.php';
+include_once '../dao/IDao.php';
+include_once '../classes/Utilisateur.php';
+class Utilisateur extends Database implements IDao
+{
+    private $connexion;
+    function __construct()
+    {
+        $this->connexion = new DataBase();
+    }
+    public function create($obj)
+    {
+        $query = "INSERT INTO `Utilisateur`(nom,prenom,email,`password`,id_role) VALUES(:nom,:prenom,:email,:`password`,:id_role";
+        $req = $this->connexion->getConnexion()->prepare($query);
+        $req->execute([
+            'nom' => $obj->getNom(),
+            'prenom' => $obj->getPrenom(),
+            'email' => $obj->getEmail(),
+            'password' => $obj->getPassword(),
+            'id_role' => $obj->getIdRole(),
+        ]) or die('SQL');
+        header('Location: ../view/reservation.php');
+    }
+    public function update($obj)
+    {
+        # code...
+    }
+    public function delete($id)
+    {
+        $query = "DELETE FROM `utilisateur` WHERE id_utilisateur =" . $id;
+
+        $req = $this->connexion->getConnexion()->prepare($query);
+
+        $req->execute() or die('SQL');
+
+        header('Location: ../view/reservation.php');
+    }
+
+    public function findAll()
+    {
+        $query = "select * from utilisateur";
+        $req = $this->connexion->getConnexion()->prepare($query);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function findById($id)
+    {
+        # code...
+    }
+}
