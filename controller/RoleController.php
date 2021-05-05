89664c62c3b16f75ad22d8c058d9ccf928abe29e
MohssineElAttari <14mohssine@gmail.com>
2021-05-05 14:28:01 +0000

    add role controller

--- controller/RoleController.php
+++ controller/RoleController.php
@@ -1,5 +1,20 @@
 <?php
-class RoleController
-{
+include_once '../model/RoleModel.php';
+include_once '../view/roles.php';
+$roleModel = new RoleModel();
+$roles = $roleModel->findAll();
+if (isset($_POST['ajouter'])) {
+    echo $_POST['libelle'];
+
+    $libelle = $_POST['libelle'];
     
+    echo '<script>alert("ok");</script>';
+    $roleModel->create(new Role($libelle));
 }
+if (isset($_GET['delete'])) {
+    echo '<script>alert("ok");</script>';
+    $id = $_GET['delete'];
+
+    $roleModel->delete($id);
+}
+
