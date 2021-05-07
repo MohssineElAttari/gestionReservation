28c2761e380c00196ab15ad7a5cddbeda84c67fc
MohssineElAttari <14mohssine@gmail.com>
2021-05-07 13:21:43 +0000

    git add bien controller

---
+++ controller/BienController.php
@@ -0,0 +1,20 @@
+<?php
+include_once '../model/BienModel.php';
+include_once '../view/biens.php';
+$bienModel = new BienModel();
+$biens = $bienModel->findAll();
+if (isset($_POST['ajouter'])) {
+    $nom = $_POST['nom'];
+    $vue = $_POST['vue'];
+    $prix = $_POST['prix'];
+    $image = $_POST['image'];
+    
+    echo '<script>alert("ok");</script>';
+    $bienModel->create(new Bien($nom,$vue,$prix,$image));
+}
+if (isset($_GET['delete'])) {
+    echo '<script>alert("ok");</script>';
+    $id = $_GET['delete'];
+
+    $reser->delete($id);
+}
