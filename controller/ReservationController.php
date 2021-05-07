9027e587d52e95c086c602f7fcd35f6bcfefb35c
MohssineElAttari <14mohssine@gmail.com>
2021-05-07 13:09:24 +0000

    update controller reservation

--- controller/ReservationController.php
+++ controller/ReservationController.php
@@ -1,1 +1,23 @@
 <?php
+include_once '../model/ReservationModel.php';
+include_once '../view/reservations.php';
+$reservationModel = new ReservationModel();
+$reservations = $reservationModel->findAll();
+if (isset($_POST['ajouter'])) {
+
+    $idUtilisateur = $_POST['idUtilisateur'];
+    $idBien = $_POST['idBien'];
+    $dateEntrer = $_POST['dateEntrer'];
+    $dateSortie = $_POST['dateSortie'];
+    $idPension = $_POST['idPension'];
+    $codeReservation = $_POST['codeReservation'];
+
+    echo '<script>alert("ok");</script>';
+    $reservationModel->create(new Reservation($idUtilisateur, $idBien, $idPension, $dateEntrer, $dateSortie, $codeReservation));
+}
+if (isset($_GET['delete'])) {
+    echo '<script>alert("ok");</script>';
+    $id = $_GET['delete'];
+
+    $reser->delete($id);
+}
